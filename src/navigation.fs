f82c1cb75070555981fc340d17b727431f6e6087
Julien Roncaglia <julien@roncaglia.fr>
2018-02-07 22:06:09 +0100

    Invert parameters (2.0.0)
    Also netstandard2.0

--- src/navigation.fs
+++ src/navigation.fs
@@ -1,20 +1,20 @@
 (** Custom navigation
 ------
-This port of the Elm library is about treating the address bar as an input to your program. 
+This port of the Elm library is about treating the address bar as an input to your program.
 It converts changes in the address bar into messages and provides functions for manipulation of the browser history.
-*) 
+*)
 namespace Elmish.Browser.Navigation
 
 open Fable.Import.Browser
 open Elmish
 
 (**
-#### Parser 
+#### Parser
 A function to turn the string in the address bar into data that is easier for your app to handle.
 *)
 type Parser<'a> = Location -> 'a
 
-type Navigable<'msg> = 
+type Navigable<'msg> =
     | Change of Location
     | UserMsg of 'msg
 
@@ -51,21 +51,21 @@ Treat user's program as a child component, by wrapping it and handling navigatio
 module Program =
     /// Add the navigation to a program made with `mkProgram` or `mkSimple`.
     /// urlUpdate: similar to `update` function, but receives parsed url instead of message as an input.
-    let toNavigable (parser : Parser<'a>) 
-                  (urlUpdate : 'a->'model->('model * Cmd<'msg>)) 
+    let toNavigable (parser : Parser<'a>)
+                  (urlUpdate : 'a->'model->('model * Cmd<'msg>))
                   (program : Program<'a,'model,'msg,'view>) =
-        let map (model, cmd) = 
+        let map (model, cmd) =
             model, cmd |> Cmd.map UserMsg
-        
-        let update msg model =
+
+        let update model msg =
             match msg with
             | Change location ->
                 urlUpdate (parser location) model
             | UserMsg userMsg ->
-                program.update userMsg model
+                program.update model userMsg
             |> map
 
-        let locationChanges (dispatch:Dispatch<_ Navigable>) = 
+        let locationChanges (dispatch:Dispatch<_ Navigable>) =
             let mutable lastLocation = None
             let onChange _ =
                 match lastLocation with
@@ -74,24 +74,29 @@ module Program =
                     lastLocation <- Some window.location.href
                     Change window.location |> dispatch
                 |> box
-                    
+
             window.addEventListener_popstate(unbox onChange)
             window.addEventListener_hashchange(unbox onChange)
             window.addEventListener(Navigation.NavigatedEvent, unbox onChange)
-        
+
         let subs model =
             Cmd.batch
               [ [locationChanges]
                 program.subscribe model |> Cmd.map UserMsg ]
-        
-        let init () = 
+
+        let init () =
             program.init (parser window.location) |> map
-        
-        { init = init 
+
+        { init = init
           update = update
           subscribe = subs
           onError = program.onError
-          setState = fun model dispatch -> program.setState model (UserMsg >> dispatch) 
-          view = fun model dispatch -> program.view model (UserMsg >> dispatch) }
-    
-  
+          setState = fun dispatch ->
+            let setState = program.setState (UserMsg >> dispatch)
+            fun model ->
+                setState model
+          view = fun dispatch ->
+            let view = program.view (UserMsg >> dispatch)
+            fun model ->
+                view model }
+
