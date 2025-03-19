dc8ba8d230a1e918638068266719d253ec52b011
Melvyn Laïly <melvyn.laily@gmail.com>
2025-03-19 13:39:31 +0100

    Fix tracer support for options (was crashing) (#77)
    * Fix options not being considered as DU in the tracer
    
    The previous implementation could actually crash:
    if the input is Some(DUCase), Fable.Core.Reflection.isUnion returns true
    because it does not "see" the option, and only see the wrapped DUCase.
    Reflection.FSharpType.GetUnionCases() would then attempt to get the fields
    of the input, and since option types are not considered DU under Fable,
    it would throw.
    
    * Pin build.fsx dependencies versions to fix the failing build
    
    * Convert array to list only in the tracers unit tests
    
    ---------
    
    Co-authored-by: Melvyn Laïly <mlaily@d-edge.com>

--- src/tracers.fs
+++ src/tracers.fs
@@ -5,32 +5,77 @@ open System
 open Fable.Core
 
 let getMsgNameAndFields (t: Type) (x: 'Msg) : string * obj =
+
+    let getLongName (t: Type) = sprintf "%s.%s"t.Namespace t.Name
+
+    let isOption (t: Type) = getLongName t = "Microsoft.FSharp.Core.FSharpOption`1"
+
+    let isUnion (t: Type) = isOption t || Reflection.FSharpType.IsUnion t
+
+    let getUnionFields x (t: Type) =
+        if isOption t then
+            // Options are special-cased because they are erased by Fable,
+            // and thus do not return true for IsUnion().
+            // (IsUnion() returns true for options on platforms other than Fable)
+            let value = box x
+            if isNull value then
+                {|
+                    CaseName = "None"
+                    FieldsValues = [||]
+                    FieldsTypes = [||]
+                |}
+            else
+                let value =
+                    #if FABLE_COMPILER
+                    value
+                    #else
+                    let _, field = FSharp.Reflection.FSharpValue.GetUnionFields(value, t)
+                    field[0]
+                    #endif
+                let valueType = t.GenericTypeArguments[0]
+                {|
+                    CaseName = "Some"
+                    FieldsValues = [| value |]
+                    FieldsTypes = [| {| Type = valueType; FieldName = "Value"; IsUnion = isUnion valueType |} |]
+
+                |}
+        else // Not a Union:
+            let uci, ucFields = Reflection.FSharpValue.GetUnionFields(x, t)
+            {|
+                CaseName = uci.Name
+                FieldsValues = ucFields
+                FieldsTypes =
+                    uci.GetFields()
+                    |> Array.map (fun x ->{|
+                        Type = x.PropertyType
+                        FieldName = x.Name
+                        IsUnion = isUnion x.PropertyType |})
+            |}
+    
     let rec getCaseName (t: Type) (acc: string list) (x: obj) =
-        let caseName = Reflection.getCaseName x
+        let ucInfo = getUnionFields x t
+        let acc = ucInfo.CaseName :: acc
 
-        let uci =
-            Reflection.FSharpType.GetUnionCases(t)
-            |> Array.find (fun uci -> uci.Name = caseName)
-
-        let acc = (Reflection.getCaseName x) :: acc
-        let fields = Reflection.getCaseFields x
-
-        if fields.Length = 1 && Reflection.isUnion fields.[0] then
-            getCaseName (uci.GetFields().[0].PropertyType) acc fields.[0]
-        else
+        match ucInfo.FieldsTypes with
+        | [| fieldInfo |] when fieldInfo.IsUnion -> getCaseName fieldInfo.Type acc ucInfo.FieldsValues[0]
+        | fieldsTypes ->
             // Case names are intentionally left reverted so we see
             // the most meaningful message first
             let msgName = acc |> String.concat "/"
 
             let fields =
-                (uci.GetFields(), fields)
+                (fieldsTypes, ucInfo.FieldsValues)
                 ||> Array.zip
-                |> Array.map (fun (fi, v) -> fi.Name, v)
+                |> Array.map (fun (fi, v) -> fi.FieldName, v)
+                #if FABLE_COMPILER
                 |> JsInterop.createObj
+                #else
+                |> box
+                #endif
 
             msgName, fields
 
-    if Reflection.isUnion x then
+    if isUnion t then
         getCaseName t [] x
     else
         "Msg", box x
